2cc3ce2e30b32eec049b064e0f4ce7a05fb65742
DrPandemic <bipbip500@gmail.com>
2017-01-27 20:44:35 -0500

    Adds distillery

--- mix.exs
+++ mix.exs
@@ -28,6 +28,6 @@ defmodule Masdb.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:distillery, "~> 1.0"}]
   end
 end
---
+++ rel/config.exs
@@ -0,0 +1,44 @@
+# Import all plugins from `rel/plugins`
+# They can then be used by adding `plugin MyPlugin` to
+# either an environment, or release definition, where
+# `MyPlugin` is the name of the plugin module.
+Path.join(["rel", "plugins", "*.exs"])
+|> Path.wildcard()
+|> Enum.map(&Code.eval_file(&1))
+
+use Mix.Releases.Config,
+    # This sets the default release built by `mix release`
+    default_release: :default,
+    # This sets the default environment used by `mix release`
+    default_environment: :dev
+
+# For a full list of config options for both releases
+# and environments, visit https://hexdocs.pm/distillery/configuration.html
+
+
+# You may define one or more environments in this file,
+# an environment's settings will override those of a release
+# when building in that environment, this combination of release
+# and environment configuration is called a profile
+
+environment :dev do
+  set dev_mode: true
+  set include_erts: false
+  set cookie: :"jyXcj0Auhov;%e?9O)Lot8VTG{$Ve>](RUrRIHwAQ{@57liZQ2I9[O,F)yWJ&|>p"
+end
+
+environment :prod do
+  set include_erts: true
+  set include_src: false
+  set cookie: :"K10KlHf!b&ymJa7OsL;c63I_1^iV5E`{]zpsciXk/sDjVG3d@JU;1,${G6N~iEM<"
+end
+
+# You may define one or more releases in this file.
+# If you have not set a default release, or selected one
+# when running `mix release`, the first release in the file
+# will be used by default
+
+release :masdb do
+  set version: current_version(:masdb)
+end
+
