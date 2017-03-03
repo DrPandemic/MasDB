053ea71002fb081c65645a9c773f56870bdd0c72
DrPandemic <bipbip500@gmail.com>
2017-03-03 14:20:11 -0500

    Adds remote pid query

---
+++ lib/masdb/distant_supervisor.ex
@@ -0,0 +1,17 @@
+defmodule Masdb.Node.DistantSupervisor do
+  def start_link do
+    Task.Supervisor.start_link(name: Masdb.Node.DistantSupervisor)
+  end
+
+  def get_remote_pid(remote, process) do
+    Task.await(
+      Task.Supervisor.async({Masdb.Node.DistantSupervisor, remote},
+        Masdb.Node.DistantSupervisor, :get_remote_pid_fn, [process])
+    )
+  end
+
+
+  def get_remote_pid_fn(process) do
+    Process.whereis(process)
+  end
+end
