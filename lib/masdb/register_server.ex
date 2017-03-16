ae78f2d5c3ee71a4d32577fded04a43e25c95295
Jean-Samuel Aubry-Guzzi <DrPandemic@users.noreply.github.com>
2017-03-15 22:09:29 -0400

    Adding a schema is calculated in another thread (#19)
    * Adding a schema is calculated in another thread
    
    * Fixes genserver reply logic

--- lib/masdb/register_server.ex
+++ lib/masdb/register_server.ex
@@ -9,6 +9,10 @@ defmodule Masdb.Register.Server do
     GenServer.call(__MODULE__, {:add_schema, schema})
   end
 
+  def received_add_schema(%Masdb.Schema{} = schema, nodes, answers, from) do
+    GenServer.cast(__MODULE__, {:received_add_schema, schema, nodes, answers, from})
+  end
+
   def remote_add_schema(%Masdb.Schema{} = schema) do
     GenServer.call(__MODULE__, {:remote_add_schema, schema})
   end
@@ -18,21 +22,27 @@ defmodule Masdb.Register.Server do
   end
 
   # private
-  def handle_call({:add_schema, schema}, _, state) do
+  def handle_call({:add_schema, schema}, from, state) do
     case Masdb.Register.validate_new_schema(state.schemas, schema) do
       :ok ->
-        nodes = Masdb.Node.Connection.list()
-        if Masdb.Node.Communication.has_quorum?(nodes, Masdb.Node.DistantSupervisor.query_remote_node(
-                  nodes,
-                  Masdb.Register.Server,
-                  :remote_add_schema,
-                  [schema])) do
-              {:reply, :ok, %Masdb.Register{state | schemas: [schema | state.schemas]}}
-            else
-              {:reply, :did_not_receive_quorum, state}
-            end
+        spawn fn ->
+          nodes = Masdb.Node.Connection.list()
+          a = Masdb.Node.DistantSupervisor.query_remote_node(nodes, Masdb.Register.Server, :remote_add_schema, [schema])
+          Masdb.Register.Server.received_add_schema(schema, nodes, a, from)
+        end
+        {:noreply, state}
 
       error -> {:reply, error, state}
+    end
+  end
+
+  def handle_cast({:received_add_schema, schema, nodes, answers, from}, state) do
+    if Masdb.Node.Communication.has_quorum?(nodes, answers) do
+      GenServer.reply(from, :ok)
+      {:noreply, %Masdb.Register{state | schemas: [schema | state.schemas]}}
+    else
+      GenServer.reply(from, :did_not_receive_quorum)
+      {:noreply, state}
     end
   end
 
