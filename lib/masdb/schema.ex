418d8bf8a038428547b46d9468b0598b074f57fe
Samuel Dussault <samuel_dusso@hotmail.com>
2017-03-16 09:50:57 -0400

    Nullable and Pk presence (#21)
    * Its now required to have at least one pk, it's also possible to specify whether or not the column is nullable
    
    * Fix code review

--- lib/masdb/schema.ex
+++ lib/masdb/schema.ex
@@ -1,7 +1,7 @@
 defmodule Masdb.Schema.Column do
-  @type t :: %Masdb.Schema.Column{name: String.t, type: String.t, is_pk: boolean}
+  @type t :: %Masdb.Schema.Column{name: String.t, type: String.t, is_pk: boolean, nullable: boolean}
   @enforce_keys [:name, :type]
-  defstruct [:name, :type, is_pk: false]
+  defstruct [:name, :type, is_pk: false, nullable: false]
 end
 
 defmodule Masdb.Schema do
@@ -17,8 +17,20 @@ defmodule Masdb.Schema do
   def validate(%Masdb.Schema{replication_factor: f}) when f < 0 do
     :replication_factor_limits
   end
-
-  def validate(%Masdb.Schema{}) do
+  
+  def validate(%Masdb.Schema{} = schema) do
+    validate_has_pk(schema.columns)
+  end
+  
+  defp validate_has_pk([%Masdb.Schema.Column{is_pk: true} | _]) do
     :ok
+  end
+  
+  defp validate_has_pk([%Masdb.Schema.Column{is_pk: false} | tail]) do
+    validate_has_pk(tail)
+  end
+  
+  defp validate_has_pk([]) do
+    :primary_key_is_needed
   end
 end
