3e0ab056d1b2b49d7baf1ef5fbee75d85e8819f3
DrPandemic <bipbip500@gmail.com>
2017-03-20 15:06:41 -0400

    Removes gossip supervisor

--- lib/masdb/gossip_supervisor.ex
+++
@@ -1,9 +0,0 @@
-defmodule Masdb.Gossip.Supervisor do
-  def start_link do
-    Task.Supervisor.start_link(name: __MODULE__)
-  end
-
-  def spawn_task(fun) do
-    Task.Supervisor.async_nolink(__MODULE__, fun)
-  end
-end
