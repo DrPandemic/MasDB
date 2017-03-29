38b42d1328fe435409b03b7d17e53be68b8e1451
duss2503 <samuel.dussault@usherbrooke.ca>
2017-03-28 21:57:26 -0400

    The sync time must be opened time and some infos should be optional

--- lib/masdb/data_map.ex
+++ lib/masdb/data_map.ex
@@ -1,17 +1,18 @@
 defmodule Masdb.Data.Map do
   @type node_id :: String.t
   @type time :: Masdb.Timestamp.t
+  @type opened_time :: time | nil
   @type data_map :: %{String.t => Masdb.Data.Tuple.t}
 
   @type t :: %Masdb.Data.Map{
     node_id: node_id,
     last_update_time: time,
-    last_sync_time: time,
+    last_sync_time: opened_time,
     map: data_map,
     next_id: integer
   }
-  @enforce_keys [:node_id, :last_update_time, :last_sync_time, :map, :next_id]
-  defstruct [:node_id, :last_update_time, :last_sync_time, :map, :next_id]
+  @enforce_keys [:node_id, :last_update_time]
+  defstruct [:node_id, :last_update_time, last_sync_time: nil, map: %{}, next_id: 0]
 
   def put_new(map, schema, column, value) do
     timestamp = Masdb.Timestamp.get_timestamp()
